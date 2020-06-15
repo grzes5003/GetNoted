72dc5e3297b30e574055dbfc7177ec2fcadd9ac5
grzes5003 <grzes.kulinski@gmail.com>
2020-06-15 19:37:33 +0200

    more changes to README.md, new constants added

--- frontend-react/src/constants/index.js
+++ frontend-react/src/constants/index.js
@@ -1,7 +1,7 @@
 export const constants = {
     CTX_CATEGORY: 'category',
     CTX_TASK: 'task',
-    HOST_ADDRESS: process.env.NODE_ENV === 'development' ? 'http://192.168.99.100:4000' : 'http://192.168.99.100:8080',
-    HOST_ADDRESS_WS: process.env.NODE_ENV === 'development' ? 'ws://192.168.99.100:4000' : 'ws://192.168.99.100:8080',
+    HOST_ADDRESS: process.env.NODE_ENV === 'development' ? 'http://192.168.99.100:4000' : 'https://get-noted-app.herokuapp.com',
+    HOST_ADDRESS_WS: process.env.NODE_ENV === 'development' ? 'ws://192.168.99.100:4000' : 'wss://get-noted-app.herokuapp.com',
 };
 
