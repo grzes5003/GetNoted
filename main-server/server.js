20d0d576806e0b1de8ed0de6e0d0cfd54f1f75de
grzes5003 <grzes.kulinski@gmail.com>
2020-06-15 02:10:59 +0200

    project cleanup

--- main-server/server.js
+++
@@ -1,5 +0,0 @@
-const {v4} = require('uuid');
-const {ApolloServer, MockList, gql, PubSub} = require('apollo-server');
-const { importSchema } = require('graphql-import');
-
-const typeDefs = importSchema('./schema/schema.graphql');
